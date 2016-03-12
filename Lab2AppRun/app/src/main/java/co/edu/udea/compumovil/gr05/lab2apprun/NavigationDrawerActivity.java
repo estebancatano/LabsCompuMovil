41dc9fe4e606a8b12eb6e8b8c960be7977770b94
Jorge Díaz <jlditru@gmail.com>
2016-03-12 00:33:39 -0500

    NavDrawer y Fragmentos conectados

--- Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr05/lab2apprun/NavigationDrawerActivity.java
+++ Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr05/lab2apprun/NavigationDrawerActivity.java
@@ -1,14 +1,17 @@
 package co.edu.udea.compumovil.gr05.lab2apprun;
 
+
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -59,7 +62,7 @@ public class NavigationDrawerActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         final ActionBar ab = getSupportActionBar();
         if (ab != null) {
-            // Poner ícono del drawer toggle
+            // Colocar ícono del drawer toggle
             ab.setHomeAsUpIndicator(R.drawable.ic_menu);
             ab.setDisplayHomeAsUpEnabled(true);
         }
@@ -84,20 +87,41 @@ public class NavigationDrawerActivity extends AppCompatActivity {
     }
 
     private void selectItem(String title) {
-        // Enviar título como arguemento del fragmento
-        Bundle args = new Bundle();
-        args.putString(PlaceHolderFragment.ARG_SECTION_TITLE, title);
 
-        Fragment fragment = PlaceHolderFragment.newInstance(title);
-        fragment.setArguments(args);
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager
-                .beginTransaction()
-                .replace(R.id.contenido_principal, fragment)
-                .commit();
+        Fragment fragment = null;
 
+
+        switch (title){
+            case "Eventos":
+                fragment = new EventosFragment();
+                break;
+
+            case "Perfil":
+                fragment = new PerfilFragment();
+                break;
+
+            case "Acerca de":
+                fragment = new AboutFragment();
+                break;
+
+            case "Cerrar sesión":
+                logout();
+                break;
+        }
+
+        if (fragment != null){
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.contenido_principal,fragment);
+            fragmentTransaction.commit();
+            setTitle(title); // Setear título actual
+        }
         drawerLayout.closeDrawers(); // Cerrar drawer
 
-        setTitle(title); // Setear título actual
+
+    }
+
+    public void logout(){
+        this.finish();
     }
 }
