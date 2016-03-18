b275a2686af0caf1716a9b6f70cf31293341cf7f
Esteban Cataño Escobar <esteban.catanoe@gmail.com>
2016-03-18 11:58:21 -0500

    Se implementa la opción de tomar o subir foto en el registro

--- Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr05/lab2apprun/RegistroActivity.java
+++ Lab2AppRun/app/src/main/java/co/edu/udea/compumovil/gr05/lab2apprun/RegistroActivity.java
@@ -4,6 +4,10 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +15,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import co.edu.udea.compumovil.gr05.lab2apprun.dbapprun.DBAppRun;
@@ -21,84 +31,149 @@ import co.edu.udea.compumovil.gr05.lab2apprun.dbapprun.TableColumnsUser;
 import co.edu.udea.compumovil.gr05.lab2apprun.model.Usuario;
 
 public class RegistroActivity extends AppCompatActivity implements View.OnClickListener{
+    private final static int FOTO_GALERIA = 70;
+    private final static int FOTO_CAMARA = 80;
 
     private Button btnGuardar;
+    private Button btnSubir;
     private EditText txtUsuario;
     private EditText txtContrasena;
     private EditText txtCorreo;
+    private RadioButton rbNoFoto;
+    private RadioButton rbCamara;
+    private RadioButton rbGaleria;
     private DBHelper dbHelper;
+
+    private byte[] foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        foto = null;
         btnGuardar = (Button) findViewById(R.id.btn_guardar);
+        btnSubir = (Button) findViewById(R.id.btn_subir);
         txtUsuario = (EditText) findViewById(R.id.txt_usuario);
         txtContrasena = (EditText) findViewById(R.id.txt_contrasena);
         txtCorreo = (EditText) findViewById(R.id.txt_correo);
+        rbNoFoto = (RadioButton) findViewById(R.id.rb_no_foto);
+        rbCamara = (RadioButton) findViewById(R.id.rb_camara);
+        rbGaleria = (RadioButton) findViewById(R.id.rb_galeria);
         setToolbar();
 
         dbHelper = new DBHelper(this);
 
         btnGuardar.setOnClickListener(this);
+        btnSubir.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        String usuario;
-        String contrasena;
-        String correo;
-        ArrayList<Usuario> listaUsuario;
-        // Extraer los datos ingresados
-        usuario = txtUsuario.getText().toString();
-        contrasena = txtContrasena.getText().toString();
-        correo = txtCorreo.getText().toString();
-        boolean existeUsuario = false;
-        // Verificar que se ingresen los datos
-        if ("".compareTo(usuario) == 0) {
-            String mensaje = getResources().getString(R.string.no_texto) + ": " +
-                    getResources().getString(R.string.usuario);
-            Snackbar.make(v, mensaje, Snackbar.LENGTH_SHORT).show();
-            return;
-        }
-        if ("".compareTo(contrasena) == 0) {
-            String mensaje = getResources().getString(R.string.no_texto) + ": " +
-                    getResources().getString(R.string.contrasena);
-            Snackbar.make(v, mensaje, Snackbar.LENGTH_SHORT).show();
-            return;
-        }
-        if ("".compareTo(correo) == 0) {
-            String mensaje = getResources().getString(R.string.no_texto) + ": " +
-                    getResources().getString(R.string.correo);
-            Snackbar.make(v, mensaje, Snackbar.LENGTH_SHORT).show();
-            return;
-        }
-        existeUsuario = dbHelper.consultarUsuarioRegistro(usuario);
-        if (existeUsuario) {
-            String mensaje = getResources().getString(R.string.existe_usuario);
-            Snackbar.make(v, mensaje, Snackbar.LENGTH_SHORT).show();
-            return;
-        } else {
-            // Se prepara el ContentValues para insertar el usuario
-            ContentValues values = new ContentValues();
-            values.put(TableColumnsUser.USUARIO, usuario);
-            values.put(TableColumnsUser.CONTRASEÑA, contrasena);
-            values.put(TableColumnsUser.EMAIL, correo);
-            values.put(TableColumnsUser.FOTO, (byte[]) null);
-            dbHelper.insertar(DBAppRun.TABLE_USERS, values);
+        switch (v.getId()){
+            case R.id.btn_guardar:
+                String usuario;
+                String contrasena;
+                String correo;
+                ArrayList<Usuario> listaUsuario;
+                // Extraer los datos ingresados
+                usuario = txtUsuario.getText().toString();
+                contrasena = txtContrasena.getText().toString();
+                correo = txtCorreo.getText().toString();
+                boolean existeUsuario = false;
+                // Verificar que se ingresen los datos
+                if ("".compareTo(usuario) == 0) {
+                    String mensaje = getResources().getString(R.string.no_texto) + ": " +
+                            getResources().getString(R.string.usuario);
+                    Snackbar.make(v, mensaje, Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if ("".compareTo(contrasena) == 0) {
+                    String mensaje = getResources().getString(R.string.no_texto) + ": " +
+                            getResources().getString(R.string.contrasena);
+                    Snackbar.make(v, mensaje, Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if ("".compareTo(correo) == 0) {
+                    String mensaje = getResources().getString(R.string.no_texto) + ": " +
+                            getResources().getString(R.string.correo);
+                    Snackbar.make(v, mensaje, Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                existeUsuario = dbHelper.consultarUsuarioRegistro(usuario);
+                if (existeUsuario) {
+                    String mensaje = getResources().getString(R.string.existe_usuario);
+                    Snackbar.make(v, mensaje, Snackbar.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    // Se prepara el ContentValues para insertar el usuario
+                    ContentValues values = new ContentValues();
+                    values.put(TableColumnsUser.USUARIO, usuario);
+                    values.put(TableColumnsUser.CONTRASEÑA, contrasena);
+                    values.put(TableColumnsUser.EMAIL, correo);
+                    values.put(TableColumnsUser.FOTO, foto);
+                    dbHelper.insertar(DBAppRun.TABLE_USERS, values);
 
-            String mensaje = getResources().getString(R.string.usuario_registrado);
-            Toast.makeText(this,mensaje,Toast.LENGTH_LONG).show();
+                    String mensaje = getResources().getString(R.string.usuario_registrado);
+                    Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
 
-            Intent intentResult = new Intent();
-            intentResult.putExtra(InicioActivity.TAG_USUARIO, usuario);
-            intentResult.putExtra(InicioActivity.TAG_CONTRASENA, contrasena);
-            intentResult.putExtra(InicioActivity.TAG_CORREO, correo);
-            // Activity finished ok, return the data
-            setResult(RESULT_OK, intentResult);
-            this.finish();
+                    Intent intentResult = new Intent();
+                    intentResult.putExtra(InicioActivity.TAG_USUARIO, usuario);
+                    intentResult.putExtra(InicioActivity.TAG_CONTRASENA, contrasena);
+                    intentResult.putExtra(InicioActivity.TAG_CORREO, correo);
+                    // Activity finished ok, return the data
+                    setResult(RESULT_OK, intentResult);
+                    this.finish();
+                }
+                    break;
+            case R.id.btn_subir:
+                if (!rbNoFoto.isChecked()){
+                    Intent intent = null;
+                    int codigo = 0;
+                    if (rbCamara.isChecked()){
+                        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        codigo = FOTO_CAMARA;
+                    }else if (rbGaleria.isChecked()){
+                        intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                        codigo = FOTO_GALERIA;
+                    }
+                    startActivityForResult(intent, codigo);
+                }
+                break;
         }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if (requestCode == FOTO_GALERIA){
+                Uri imagenSeleccionada = data.getData();
+                InputStream is = null;
+                try {
+                    is = getContentResolver().openInputStream(imagenSeleccionada);
+                    BufferedInputStream bis = new BufferedInputStream(is);
+                    Bitmap bitmap = BitmapFactory.decodeStream(bis);
+                    foto = getBitmapAsByteArray(bitmap);
+                } catch (FileNotFoundException e) {}
+            }else if (requestCode == FOTO_CAMARA){
+                if(data != null){
+                    Uri imagenSeleccionada = data.getData();
+                    InputStream is = null;
+                    try {
+                        is = getContentResolver().openInputStream(imagenSeleccionada);
+                        BufferedInputStream bis = new BufferedInputStream(is);
+                        Bitmap bitmap = BitmapFactory.decodeStream(bis);
+                        foto = getBitmapAsByteArray(bitmap);
+                        ImageView im = (ImageView) findViewById(R.id.img_view);
+                        im.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {}
+                }
+            }
+        }
+
+
     }
 
     private void setToolbar() {
@@ -112,5 +187,11 @@ public class RegistroActivity extends AppCompatActivity implements View.OnClickL
             getSupportActionBar().setIcon(R.mipmap.logo_grupo5);
 
         }
+    }
+
+    public static byte[] getBitmapAsByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, outputStream);
+        return outputStream.toByteArray();
     }
 }
