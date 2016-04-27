649e236b01d00fcaba7d6633b2da651b5549cee4
Jorge Díaz <jlditru@gmail.com>
2016-04-26 20:48:41 -0500

    Initial commit Lab4

---
+++ Lab4Weather/app/src/main/java/co/edu/udea/compumovil/gr05/lab4weather/MainActivity.java
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.gr05.lab4weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
