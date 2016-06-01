36edbf9aa2bdaba803aaa752d365c502cdebb8cf
Esteban Cataño Escobar <esteban.catanoe@udea.edu.co>
2016-06-01 19:32:35 +0000

    Busca la ciudad de la ubicación al iniciar

--- Lab6Wheater/www/js/controllers.js
+++ Lab6Wheater/www/js/controllers.js
@@ -1,49 +1,64 @@
-var API_KEY = '5d99375cb63bf3ace57f574cc67fa8aa';
-var URL = 'http://api.openweathermap.org/data/2.5/weather';
 var URL_CLIMA = 'http://openweathermap.org/img/w/';
-var PARAMS ='&lang=es&units=metric';
+var latitud = 25.26;
+var longitud = 55.3;
+console.log('Inicio');
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}else{
+    alert('Lo sentimos, su navegador no soporta servicios de geolocalizacion');
+}
+
 angular.module('app.controllers', [])
   
-.controller('estadoDelClimaCtrl', function($scope, Clima, ImagenClima) {
+.controller('estadoDelClimaCtrl', function($scope, Clima, ClimaGPS) {
+    $scope.mostrarClimaGPS = function(){
+        ClimaGPS.buscarClimaGPS(latitud,longitud).success(function(date){
+            console.log("Aqui estoy");
+            console.log(date);
+            mostrarDatos(date);
+       
+        });
+    }
     $scope.mostrarClima = function(){
         Clima.buscarClima($scope.ciudad).success(function(date){
-            console.log(date);
-            if(date.cod == 200){
-                document.getElementById("headerTituloTemperatura").innerHTML = 'Temperatura';
-                document.getElementById("headerTituloHumedad").innerHTML = 'Humedad';
-                document.getElementById("headerTituloDescripcion").innerHTML = 'Descripción';
-                document.getElementById("headerTemperatura").innerHTML = ''.concat(date.main.temp).concat(' °C');
-                document.getElementById("headerHumedad").innerHTML = ''.concat(date.main.humidity).concat(' %');
-                document.getElementById("headerDescripcion").innerHTML = date.weather[0].description;
-                $scope.ciudad = date.name;
-                var imagen = date.weather[0].icon;
-                document.getElementById('imagenClima').src = URL_CLIMA.concat(imagen).concat('.png');
-            }else{
-                $scope.respuesta = 'La ciudad ingresada no fue encontrada';
-                document.getElementById("headerTituloTemperatura").innerHTML = '';
-                document.getElementById("headerTituloHumedad").innerHTML = '';
-                document.getElementById("headerTituloDescripcion").innerHTML = '';
-                document.getElementById("headerTemperatura").innerHTML = '';
-                document.getElementById("headerHumedad").innerHTML = '';
-                document.getElementById("headerDescripcion").innerHTML = '';
-                document.getElementById('imagenClima').src = '';
-            }
+           mostrarDatos(date);
         });
-        
     };
-})
-
-.service('Clima',function($http){
-	this.buscarClima = function(ciudad){
-		return $http({
-			url: URL, 
-            method: "GET",
-            params: {q: ciudad,
-                lang: 'es',
-                units: 'metric',
-                appid: API_KEY
-            }
-		});
-	};
+    function mostrarDatos(date){
+        if(date.cod == 200){
+            document.getElementById("headerTituloTemperatura").innerHTML = 'Temperatura';
+            document.getElementById("headerTituloHumedad").innerHTML = 'Humedad';
+            document.getElementById("headerTituloDescripcion").innerHTML = 'Descripción';
+            document.getElementById("headerTemperatura").innerHTML = ''.concat(date.main.temp).concat(' °C');
+            document.getElementById("headerHumedad").innerHTML = ''.concat(date.main.humidity).concat(' %');
+            document.getElementById("headerDescripcion").innerHTML = date.weather[0].description;
+            $scope.ciudad = date.name;
+            var imagen = date.weather[0].icon;
+            document.getElementById('imagenClima').src = URL_CLIMA.concat(imagen).concat('.png');
+        }else{
+            $scope.respuesta = 'La ciudad ingresada no fue encontrada';
+            document.getElementById("headerTituloTemperatura").innerHTML = '';
+            document.getElementById("headerTituloHumedad").innerHTML = '';
+            document.getElementById("headerTituloDescripcion").innerHTML = '';
+            document.getElementById("headerTemperatura").innerHTML = '';
+            document.getElementById("headerHumedad").innerHTML = '';
+            document.getElementById("headerDescripcion").innerHTML = '';
+            document.getElementById('imagenClima').src = '';
+        }
+    }
 });
+
+function onSuccess(position){
+    latitud = position.coords.latitude;
+    longitud = position.coords.longitude;
+    console.log('En el GPS');
+    angular.element(document.getElementById('controllerClima')).scope().mostrarClimaGPS();
+    
+} 
+
+function onError(error){
+    alert('codigo: ' + error.code + '\n' + 
+        'mensaje: ' + error.message + '\n');
+}
  
--- Lab6Wheater/www/js/services.js
+++ Lab6Wheater/www/js/services.js
@@ -1,10 +1,34 @@
+var API_KEY = '5d99375cb63bf3ace57f574cc67fa8aa';
+var URL = 'http://api.openweathermap.org/data/2.5/weather';
+
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
-
-}])
-
-.service('BlankService', [function(){
-
-}]);
+.service('Clima',function($http){
+	this.buscarClima = function(ciudad){
+		return $http({
+			url: URL, 
+            method: "GET",
+            params: {q: ciudad,
+                lang: 'es',
+                units: 'metric',
+                appid: API_KEY
+            }
+		});
+	};
+})
+.service('ClimaGPS',function($http){
+    this.buscarClimaGPS = function(latitud,longitud){
+        return $http({
+			url: URL, 
+            method: "GET",
+            params: {
+                lat: latitud,
+                lon: longitud,
+                lang: 'es',
+                units: 'metric',
+                appid: API_KEY
+            }
+		});
+    }
+});
 
